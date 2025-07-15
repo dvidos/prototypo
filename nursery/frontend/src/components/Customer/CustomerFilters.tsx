9039968d9a028d315c33243542445480bd696012
Dimitris Vidos <dvidos@gmail.com>
2025-07-15 13:28:44 -0400

    Debounced filters

--- nursery/frontend/src/components/Customer/CustomerFilters.tsx
+++ nursery/frontend/src/components/Customer/CustomerFilters.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export interface CustomerFilterValues {
   firstName: string;
@@ -12,11 +12,24 @@ interface CustomerFiltersProps {
 }
 
 const CustomerFilters: React.FC<CustomerFiltersProps> = React.memo(({ filters, onChange }) => {
+  const [localFilters, setLocalFilters] = useState(filters);
+
+  useEffect(() => {
+    setLocalFilters(filters);
+  }, [filters]);
+
+  const DEBOUNCE_DELAY_MSECS = 300;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => { onChange(localFilters); }, DEBOUNCE_DELAY_MSECS);
+    return () => clearTimeout(timeout);
+  }, [localFilters, onChange]);
+
   const handleChange = (field: keyof CustomerFilterValues, value: string) => {
-    onChange({
+    setLocalFilters((prev) => ({
       ...filters,
       [field]: value,
-    });
+    }));
   };
 
   return (
@@ -26,7 +39,7 @@ const CustomerFilters: React.FC<CustomerFiltersProps> = React.memo(({ filters, o
       <input
         type="text"
         placeholder="First name contains..."
-        value={filters.firstName}
+        value={localFilters.firstName}
         onChange={(e) => handleChange("firstName", e.target.value)}
         style={{ marginLeft: "1em"}}
       />
@@ -34,7 +47,7 @@ const CustomerFilters: React.FC<CustomerFiltersProps> = React.memo(({ filters, o
       <input
         type="text"
         placeholder="Email contains..."
-        value={filters.email}
+        value={localFilters.email}
         onChange={(e) => handleChange("email", e.target.value)}
         style={{ marginLeft: "1em"}}
       />
@@ -42,7 +55,7 @@ const CustomerFilters: React.FC<CustomerFiltersProps> = React.memo(({ filters, o
       <input
         type="text"
         placeholder="Address contains..."
-        value={filters.address}
+        value={localFilters.address}
         style={{ marginLeft: "1em"}}
         onChange={(e) => handleChange("address", e.target.value)}
       />
